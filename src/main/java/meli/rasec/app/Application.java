0af06fe8cc2e658bd276623f65086817e2e26937
12asec <cesarnt@gmail.com>
2024-05-20 18:31:48 -0400

    commit inicial

---
+++ src/main/java/meli/rasec/app/Application.java
@@ -0,0 +1,12 @@
+package meli.rasec.app;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application{
+
+    static void main (String args []){
+
+    }
+}
+
